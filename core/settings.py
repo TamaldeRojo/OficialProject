8e394531ba31a0cfcc65fcbf37f6bb41398c341f
Josué <josue.medina9991@gmail.com>
2023-01-22 16:07:36 -0600

    commit

--- core/settings.py
+++ core/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '6bgk3$s5s)fqo5eqh596dp_0#6l9-jn%tofrz6ad6i#wuzjbd(' #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True #os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
